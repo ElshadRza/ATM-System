09c151b2c628001e45557b66ddbeeeb770405653
Elshad Rzaguliyev <118837671+ElshadRza@users.noreply.github.com>
2023-03-02 19:06:13 +0400

    Update main.js

--- ATM System/js/main.js
+++ ATM System/js/main.js
@@ -1,7 +1,7 @@
-const greetingPrompt = Number(prompt(
+let greetingPrompt = Number(prompt(
   "Hi you have 1000 dollars in your balance.Press 1 if you would like to withdraw or press 2 to deposit."
 ));
-const balance = 1000;
+let balance = 1000;
 choiceCheck(greetingPrompt,balance);
 
 
@@ -11,7 +11,7 @@ function choiceCheck(greetingPrompt,balance) {
       withdrawFromBalance(greetingPrompt,balance);
   } 
   else if(greetingPrompt === 2){
-    depositToBalance();
+    depositToBalance(greetingPrompt,balance);
   }
   
   else {
@@ -20,28 +20,26 @@ function choiceCheck(greetingPrompt,balance) {
 }
 
 function withdrawFromBalance(greetingPrompt,balance) {
-    const withdrawAmount = prompt("How much would you like to withdraw?");
+    let withdrawAmount = prompt("How much would you like to withdraw?");
     
-    if(greetingPrompt===1){
+   
   if (withdrawAmount > 1000 || withdrawAmount < 0) {
     withdrawAmount = prompt("Please type in a number between 0 and 1000:");
   } 
   else{
     alert(
-      "Operation successfull you have"`${balance - withdrawAmount}`,
-      " dollars left."
+      `Operation successfull you have ${balance - withdrawAmount}  dollars left.`
     );
   }
 
 }
-}
 
-function depositToBalance(balance) {
-  const depositAmount = prompt("How much would you like to deposit?");
+function depositToBalance(greetingPrompt,balance) {
+  let depositAmount = prompt("How much would you like to deposit?");
   if (depositAmount < 1) {
     depositAmount = prompt("Please type in a correct number greater than 1:");
   }
    else {
-    alert("Operation successful.Your balance is"`${balance + depositAmount}`);
+    alert(`Operation successful.Your balance is ${Number(balance) + Number(depositAmount)}`);
   }
 }
